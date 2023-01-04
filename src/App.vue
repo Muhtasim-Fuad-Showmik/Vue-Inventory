0a3ca450ba6a9c567447d469e731705c819a86fc
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2023-01-04 19:02:41 +0600

    Added tailwind config and tailwind header styles

--- src/App.vue
+++ src/App.vue
@@ -4,82 +4,41 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+  <header class="h-full flex flex-col bg-white">
+    <div class="bg-brandeis-blue">
+      <a href="/" class="w-full inline-block pl-2 pt-2 pr-4 pb-5 bg-white rounded-tr-lg ">
+        <div class="flex">
+          <img src="/Inventory Logo.png" alt="Inventory Management Logo" width="40px" />
+          <span class="font-Jost font-bold text-xl text-black underline decoration-white  flex flex-col ml-5">
+            <p>Inventory</p>
+            <p>Management</p>
+          </span>
+        </div>
+      </a>
+    </div>
+    <ul class="min-h-full m-0 pl-0.5 pt-2 list-none flex-1 text-left bg-brandeis-blue rounded-tr-lg">
+      <li class="group block m-0 pb-30 border-y-2 transition-all border-transparent hover:border-[#1b82f7]">
+        <RouterLink class="block bg-transparent px-4 py-2 no-underline text-white transition-all group-hover:drop-shadow-std group-hover:translate-x-{-2px} group-hover:-translate-y-0.5" to="/">Dashboard</RouterLink>
+      </li>
+      <li class="group block m-0 pb-30 border-y-2 transition-all border-transparent hover:border-[#1b82f7]">
+        <RouterLink class="block bg-transparent px-4 py-2 no-underline text-white transition-all group-hover:drop-shadow-std group-hover:translate-x-{-2px} group-hover:-translate-y-0.5" to="/about">Products</RouterLink>
+      </li>
+      <li class="group block m-0 pb-30 border-y-2 transition-all border-transparent hover:border-[#1b82f7]">
+        <RouterLink class="block bg-transparent px-4 py-2 no-underline text-white transition-all group-hover:drop-shadow-std group-hover:translate-x-{-2px} group-hover:-translate-y-0.5" to="/">Procurement</RouterLink>
+      </li>
+      <li class="group block m-0 pb-30 border-y-2 transition-all border-transparent hover:border-[#1b82f7]">
+        <RouterLink class="block bg-transparent px-4 py-2 no-underline text-white transition-all group-hover:drop-shadow-std group-hover:translate-x-{-2px} group-hover:-translate-y-0.5" to="/">Inventory</RouterLink>
+      </li>
+      <li class="group block m-0 pb-30 border-y-2 transition-all border-transparent hover:border-[#1b82f7]">
+        <RouterLink class="block bg-transparent px-4 py-2 no-underline text-white transition-all group-hover:drop-shadow-std group-hover:translate-x-{-2px} group-hover:-translate-y-0.5" to="/">Analytics</RouterLink>
+      </li>
+    </ul>
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
     </div>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				'brandeis-blue': '#006ce9',
+				'spiro-disco-ball': '#18b6f6',
+				'lavender': '#ac7ff4',
+				'plump-purple': '#713fc2',
+				'selective-yellow': '#ffb703',
+				'caribbean-green': '#02c39a',
+			},
+			dropShadow: {
+				'std': '2px 2px 5px rgba(0, 0, 0, 1)',
+			}
+		},
+		fontFamily: {
+			Jost: ["Jost, sans-serif"],
+		},
+		screens: {
+			sm: "640px",
+			md: "768px",
+		}
+	},
+	plugins: [],
 }
