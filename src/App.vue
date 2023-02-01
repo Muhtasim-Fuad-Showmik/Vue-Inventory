8a4ed19a6ebfb350d3455a19f6b472579056ed17
Muhtasim-Fuad-Showmik <muhtasimfuadshowmik@gmail.com>
2023-02-01 18:58:56 +0600

    Added responsive behavior to the navigation pane and the app logo

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@ import { RouterLink, RouterView } from 'vue-router';
 
 <template>
     <main class="h-[90vh] my-0 mx-auto bg-white rounded shadow-[0_0_130px_-50px_rgba(0,108,233,1)] overflow-auto w-full">
-        <div class="flex flex-row w-full min-h-[90vh] overflow-auto">
-            <div class="min-w-[200px] min-h-[90vh]">
+        <div class="flex flex-row w-full min-h-[90vh]">
+            <div class="max-[600px]:min-w-[80px] min-w-[200px] min-h-[90vh]">
                 <header class="flex flex-col min-h-full bg-white">
                     <div class="bg-brandeis-blue">
                         <a href="/" class="inline-block w-full p-5 bg-white rounded-bl-lg">
@@ -13,7 +13,7 @@ import { RouterLink, RouterView } from 'vue-router';
                                 <img src="/Inventory Logo.png" alt="Inventory Management Logo"
                                     class="object-cover w-10 h-12" />
                                 <span
-                                    class="flex flex-col ml-2 text-xl font-bold text-black underline font-Sen decoration-white">
+                                    class="flex flex-col ml-2 text-xl font-bold text-black underline font-Sen decoration-white max-[600px]:hidden">
                                     <p>Inventory</p>
                                     <p>Management</p>
                                 </span>
@@ -24,32 +24,57 @@ import { RouterLink, RouterView } from 'vue-router';
                         <li
                             class="group block m-0 py-4 px-2.5 border-y transition-all border-transparent hover:border-[#1b82f7]">
                             <RouterLink
-                                class="block bg-transparent px-4 py-2 no-underline text-white transition-all group-hover:drop-shadow-std group-hover:translate-x-{-2px} group-hover:-translate-y-0.5"
-                                to="/">Dashboard</RouterLink>
+                                class="block bg-transparent px-4 py-2 no-underline text-white transition-all group-hover:drop-shadow-std group-hover:translate-x-{-2px} group-hover:-translate-y-0.5 flex items-center"
+                                to="/">
+                                <span>
+                                    <i class="fa-solid fa-gauge text-2xl mr-5"></i>
+                                </span>
+                                <span class="max-[600px]:hidden">Dashboard</span>
+                            </RouterLink>
                         </li>
                         <li
                             class="group block m-0 py-4 px-2.5 border-y transition-all border-transparent hover:border-[#1b82f7]">
                             <RouterLink
-                                class="block bg-transparent px-4 py-2 no-underline text-white transition-all group-hover:drop-shadow-std group-hover:translate-x-{-2px} group-hover:-translate-y-0.5"
-                                to="/">Products</RouterLink>
+                                class="block bg-transparent px-4 py-2 no-underline text-white transition-all group-hover:drop-shadow-std group-hover:translate-x-{-2px} group-hover:-translate-y-0.5 flex items-center"
+                                to="/">
+                                <span>
+                                    <i class="fa-solid fa-basket-shopping text-2xl mr-5"></i>
+                                </span>
+                                <span class="max-[600px]:hidden">Products</span>
+                            </RouterLink>
                         </li>
                         <li
                             class="group block m-0 py-4 px-2.5 border-y transition-all border-transparent hover:border-[#1b82f7]">
                             <RouterLink
-                                class="block bg-transparent px-4 py-2 no-underline text-white transition-all group-hover:drop-shadow-std group-hover:translate-x-{-2px} group-hover:-translate-y-0.5"
-                                to="/procurements">Procurement</RouterLink>
+                                class="block bg-transparent px-4 py-2 no-underline text-white transition-all group-hover:drop-shadow-std group-hover:translate-x-{-2px} group-hover:-translate-y-0.5 flex items-center"
+                                to="/procurements">
+                                <span>
+                                    <i class="fa-solid fa-truck-ramp-box text-2xl mr-5"></i>
+                                </span>
+                                <span class="max-[600px]:hidden">Procurement</span>
+                            </RouterLink>
                         </li>
                         <li
                             class="group block m-0 py-4 px-2.5 border-y transition-all border-transparent hover:border-[#1b82f7]">
                             <RouterLink
-                                class="block bg-transparent px-4 py-2 no-underline text-white transition-all group-hover:drop-shadow-std group-hover:translate-x-{-2px} group-hover:-translate-y-0.5"
-                                to="/">Inventory</RouterLink>
+                                class="block bg-transparent px-4 py-2 no-underline text-white transition-all group-hover:drop-shadow-std group-hover:translate-x-{-2px} group-hover:-translate-y-0.5 flex items-center"
+                                to="/">
+                                <span>
+                                    <i class="fa-solid fa-warehouse text-2xl mr-5"></i>
+                                </span>
+                                <span class="max-[600px]:hidden">Inventory</span>
+                            </RouterLink>
                         </li>
                         <li
                             class="group block m-0 py-4 px-2.5 border-y transition-all border-transparent hover:border-[#1b82f7]">
                             <RouterLink
-                                class="block bg-transparent px-4 py-2 no-underline text-white transition-all group-hover:drop-shadow-std group-hover:translate-x-{-2px} group-hover:-translate-y-0.5"
-                                to="/">Analytics</RouterLink>
+                                class="block bg-transparent px-4 py-2 no-underline text-white transition-all group-hover:drop-shadow-std group-hover:translate-x-{-2px} group-hover:-translate-y-0.5 flex items-center"
+                                to="/">
+                                <span>
+                                    <i class="fa-solid fa-chart-pie text-2xl mr-5"></i>
+                                </span>
+                                <span class="max-[600px]:hidden">Analytics</span>
+                            </RouterLink>
                         </li>
                     </ul>
                 </header>
